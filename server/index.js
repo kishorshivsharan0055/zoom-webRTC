a5652ea8d5d7b60b1d0ae8c8b6cee1545a19cd15
Kishor Shivsharan <kishorshivsharan8796@gmail.com>
2024-04-08 08:43:32 +0530

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
+
+const io = new Server();
+const app = express();
+
+app.use(bodyParser.json());
+
+io.on("connection", (socket) => {});
+
+app.listen(8000, () => console.log("Server up & running on port 8000"));
+io.listen(8001);
